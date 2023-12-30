96ae9fff7b034e444939d72c5ca24499e6bc00db
flyck <felix.brilej@googlemail.com>
2023-12-30 15:23:59 +0100

    chore: make the user info button smarter

--- frontend/components/user-details.tsx
+++ frontend/components/user-details.tsx
@@ -1,20 +1,36 @@
 import React, { useEffect } from 'react';
 import { Button } from "@/components/ui/button"
+import { useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation'
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 
 const UserDetails: React.FC<any> = () => {
   //https://stackoverflow.com/questions/72673362/error-text-content-does-not-match-server-rendered-html
   const [hydrated, setHydrated] = React.useState(false);
+  const router = useRouter();
+  const pathName = usePathname()
+
   useEffect(() => {
     setHydrated(true);
   }, []);
 
   return (<>
     <div className="text-right">
-      <Button className="inline-block bg-gray-900 border-gray-800" onClick={() => window.location.assign("/editUser")}>
+      <Button className="inline-block bg-gray-900 border-gray-800" onClick={() => navigate(pathName, router)}>
         {!hydrated ? null : getUserName()}
       </Button>
     </div >
   </>)
+}
+
+// allow double-click on the button in case you dont want to make any changes
+function navigate(path: string | null, router: AppRouterInstance) {
+  if (path && path === "/editUser") {
+    router.back()
+    return
+  }
+
+  window.location.assign("/editUser")
 }
 
 function getUserName() {
