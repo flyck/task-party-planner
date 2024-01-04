6399333503ec4f59c6711c14430976ea56a6cecf
flyck <felix.brilej@googlemail.com>
2024-01-04 09:34:50 +0100

    chore: move party resolvers to their own file

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -4,6 +4,7 @@ import { Construct } from 'constructs';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 import { join } from "path";
+import { createPartyResolvers } from './backend-stack.parties';
 
 interface MultiStackProps extends StackProps {
   projectName: string
@@ -28,6 +29,23 @@ export class BackendStack extends cdk.Stack {
 
     partyTable.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
 
+    const participantTable = new dynamodb.Table(this, 'participants', {
+      tableName: `${projectName}-participants`,
+      partitionKey: {
+        name: 'partyId',
+        type: dynamodb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecovery: true,
+    });
+
+    partyTable.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+
     // AppSync API
     const api = new appsync.GraphqlApi(this, 'Api', {
       name: projectName,
@@ -46,51 +64,6 @@ export class BackendStack extends cdk.Stack {
     const partyDataSource = api.addDynamoDbDataSource('partyDataSource', partyTable);
     const noneDataSource = api.addNoneDataSource('None');
 
-    partyDataSource.createResolver("createParty", {
-      typeName: "Mutation",
-      fieldName: "createParty",
-      runtime: appsync.FunctionRuntime.JS_1_0_0,
-      code: appsync.Code.fromAsset(join(__dirname, "./resolvers/createParty.js"))
-    })
-
-    partyDataSource.createResolver("getParty", {
-      typeName: 'Query',
-      fieldName: 'getParty',
-      requestMappingTemplate: appsync.MappingTemplate.dynamoDbGetItem("id", "id"),
-      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
-    });
-
-    // Make the update respect existing data. First resolver is a javascript resolver as well since
-    // javascript resolvers and templates cannot be mixed within the same pipeline
-    const UpdatePartyF1 = new appsync.AppsyncFunction(this, "updatePartyF1", {
-      api,
-      name: "UpdatePartyGet",
-      dataSource: partyDataSource,
-      runtime: appsync.FunctionRuntime.JS_1_0_0,
-      code: appsync.Code.fromAsset(join(__dirname, "./resolvers/getParty.js")),
-    })
-    // cannot seem to run two operations in the same unit resolver even with javascript
-    const UpdatePartyF2 = new appsync.AppsyncFunction(this, "updatePartyF2", {
-      api,
-      name: "UpdatePartyPut",
-      dataSource: partyDataSource,
-      runtime: appsync.FunctionRuntime.JS_1_0_0,
-      code: appsync.Code.fromAsset(join(__dirname, "./resolvers/updateParty.js"))
-    })
-    new appsync.Resolver(this, "UpdatePipeline", {
-      api,
-      typeName: 'Mutation',
-      fieldName: 'updateParty',
-      pipelineConfig: [UpdatePartyF1, UpdatePartyF2],
-      requestMappingTemplate: appsync.MappingTemplate.fromString('{}'),
-      responseMappingTemplate: appsync.MappingTemplate.fromString('$util.toJson($ctx.result)'),
-    })
-
-    partyDataSource.createResolver("deleteParty", {
-      typeName: 'Mutation',
-      fieldName: 'deleteParty',
-      requestMappingTemplate: appsync.MappingTemplate.dynamoDbDeleteItem("id", "id"),
-      responseMappingTemplate: appsync.MappingTemplate.dynamoDbResultItem(),
-    });
+    createPartyResolvers(this, api, partyDataSource);
   }
 }
